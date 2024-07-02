939d75d3d0f057f57f39ffd7c3b77e12b284aa2b
hankrugg <hankrugg2@gmail.com>
2024-07-02 11:24:18 -0600

    moved yardmarkers and hashmarks below markers

--- Visualizations.py
+++ Visualizations.py
@@ -29,144 +29,127 @@ def animatePlay(games: pd.DataFrame, players: pd.DataFrame, plays: pd.DataFrame,
     homeTeam = games.query('gameId == @gameId')['homeTeamAbbr'].unique()[0]
     visitingTeam = games.query('gameId == @gameId')['visitorTeamAbbr'].unique()[0]
 
-    frames = []  # Create an empty list to store the frame data for animation
-    for frame_id in tracking['frameId'].unique():  # Iterate over unique frameIds
-        frame_data = go.Scatter(
-            x=tracking.query('frameId == @frame_id')['x'],  # X-coords of all the players and football at this frame
-            y=tracking.query('frameId == @frame_id')['y'],  # X-coords of all the players and football at this frame
+    topHashmarksPlot = go.Scatter(
+        x=[i for i in range(10, 110)],
+        y=[53.3 - 23.58 for i in range(10, 110)],
+        mode="text",
+        text=['|' for i in range(10, 110)],
+        textfont=dict(color="white", size=35, family="Courier New"),
+        showlegend=False
+    )
+
+    topYardMarkersPlot = go.Scatter(
+        x=[20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+        y=[43.5 for _ in range(10)],
+        mode="text",
+        text=['10', '20', '30', '40', '50', '40', '30', '20', '10'],
+        textfont=dict(color="white", size=35, family="Courier New"),
+        showlegend=False
+    )
+
+    bottomYardmarkersPlot = go.Scatter(
+        x=[20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+        y=[10 for _ in range(10)],
+        mode="text",
+        text=['10', '20', '30', '40', '50', '40', '30', '20', '10'],
+        textfont=dict(color="white", size=35, family="Courier New"),
+        showlegend=False
+    )
+
+    bottomHashmarksPlot = go.Scatter(
+        x=[i for i in range(10, 110)],
+        y=[23.58 for i in range(10, 110)],
+        mode="text",
+        text=['|' for i in range(10, 110)],
+        textfont=dict(color="white", size=35, family="Courier New"),
+        showlegend=False
+    )
+
+    # Create the frames
+    frames = []
+    for frame_id in tracking['frameId'].unique():
+        playersScatterPlot = go.Scatter(
+            x=tracking.query('frameId == @frame_id')['x'],
+            y=tracking.query('frameId == @frame_id')['y'],
             text=tracking.query('frameId == @frame_id')['jerseyNumber'],
-            # Jersey of all the players and football at this frame
             mode="markers+text",
             marker=dict(
                 color=[nfl_teams_colors[club][0] for club in tracking.query('frameId == @frame_id')['club']],
-                # List of colors to set each marker
                 size=16,
                 line=dict(
                     color=[nfl_teams_colors[club][1] for club in tracking.query('frameId == @frame_id')['club']],
-                    # List of colors to set the outline of each marker
                     width=1
                 )
             ),
-            # Details of jersey number text
             textfont=dict(
                 family="Arial",
                 size=8,
                 color="white"
-            ),
+            )
         )
-        # Make a plotly frame with the data and append it to the comprehensive frame list
-        frame = go.Frame(data=[frame_data])
+
+        frame = go.Frame(
+            data=[topHashmarksPlot, bottomHashmarksPlot, topYardMarkersPlot, bottomYardmarkersPlot, playersScatterPlot])
         frames.append(frame)
 
+    # Create the figure
     fig = go.Figure(
-        # Plot initial frame data at frameID of 1
         data=[
-            go.Scatter(x=tracking.query('frameId == 1')['x'],
-                       y=tracking.query('frameId == 1')['y'],
-                       text=tracking.query('frameId == 1')['jerseyNumber'],
-                       mode="markers+text",
-                       marker=dict(
-                           color=[nfl_teams_colors[club][0] for club in tracking.query('frameId == 1')['club']],
-                           # List of colors to set each marker
-                           size=16,
-                           line=dict(
-                               color=[nfl_teams_colors[club][1] for club in
-                                      tracking.query('frameId == 1')['club']],
-                               # List of colors to set the outline of each marker
-                               width=1
-                           )
-                       ),
-                       textfont=dict(
-                           family="Arial",
-                           size=8,
-                           color="white"
-                       ),
-                       )
+            topHashmarksPlot,
+            bottomHashmarksPlot,
+            topYardMarkersPlot,
+            bottomYardmarkersPlot,
+            go.Scatter(
+                x=tracking.query('frameId == 1')['x'],
+                y=tracking.query('frameId == 1')['y'],
+                text=tracking.query('frameId == 1')['jerseyNumber'],
+                mode="markers+text",
+                marker=dict(
+                    color=[nfl_teams_colors[club][0] for club in tracking.query('frameId == 1')['club']],
+                    size=16,
+                    line=dict(
+                        color=[nfl_teams_colors[club][1] for club in tracking.query('frameId == 1')['club']],
+                        width=1
+                    )
+                ),
+                textfont=dict(
+                    family="Arial",
+                    size=8,
+                    color="white"
+                )
+            )
         ],
-
-        # Details of the plot
         layout=go.Layout(
             plot_bgcolor='green',
-            # X-axis grid to show the yard-lines
-            xaxis=dict(range=[0, 120], autorange=False, zeroline=False, showgrid=True, showticklabels=False, gridwidth=2, tickwidth=5,
-                       tickvals=list(range(10, 111, 5))),
-            # Don't show any horizontal gridlines
-            yaxis=dict(range=[0, 53.3], autorange=False, zeroline=False, showgrid=False, showticklabels=False),
+            xaxis=dict(
+                range=[0, 120],
+                autorange=False,
+                zeroline=False,
+                showgrid=True,
+                showticklabels=False,
+                gridwidth=2,
+                tickwidth=5,
+                tickvals=list(range(10, 111, 5))
+            ),
+            yaxis=dict(
+                range=[0, 53.3],
+                autorange=False,
+                zeroline=False,
+                showgrid=False,
+                showticklabels=False
+            ),
             hovermode="closest",
             showlegend=False,
-            # Frames are shown every tenth (0.1) of a second or 100 milliseconds
-            # Make a play button to start the animation
             updatemenus=[dict(
                 type="buttons",
                 buttons=[dict(label="Play",
                               method="animate",
-                              args=[None, {"frame": {"duration": 100, "redraw": True}}]
+                              args=[None, {"frame": {"duration": 100, "redraw": False}}]
                               )]
             )]
         ),
         frames=frames
     )
-    # Add the team names to the endzones
-    fig.add_trace(go.Scatter(
-        x=[5, 115],
-        y=[27, 27],
-        mode="text",
-        text=[homeTeam, visitingTeam],
-        textfont=dict(color="white",
-                      size=30),
-        showlegend=False
-    ))
-
-    # Add the yard markers 10 yards from the bottom (separated from top for readability)
-    fig.add_trace(go.Scatter(
-        x=[20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
-        y=[10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-        mode="text",
-        text=['10', '20', '30', '40', '50', '40', '30', '20', '10'],
-        textfont=dict(color="white",
-                      size=35,
-                      family="Courier New"),
-        showlegend=False
-    ))
-
-    # Add the yard markers 10 yards from the top (separated from bottom for readability)
-    fig.add_trace(go.Scatter(
-        x=[20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
-        y=[43.5, 43.5, 43.5, 43.5, 43.5, 43.5, 43.5, 43.5, 43.5, 43.5],
-        mode="text",
-        text=['10', '20', '30', '40', '50', '40', '30', '20', '10'],
-        textfont=dict(color="white",
-                      size=35,
-                      family="Courier New"),
-        showlegend=False
-    ))
-
-    # Add hashmarks to the bottom, 23.58 yards from the bottom sideline
-    fig.add_trace(go.Scatter(
-        x=[i for i in range(10, 110)],
-        y=[23.58 for i in range(10, 110)],
-        mode="text",
-        text=['|' for i in range(10, 110)],
-        textfont=dict(color="white",
-                      size=35,
-                      family="Courier New"),
-        showlegend=False
-    ))
-
-    # Add hashmarks to the top, 23.58 yards from the top sideline
-    fig.add_trace(go.Scatter(
-        x=[i for i in range(10, 110)],
-        y=[53.3 - 23.58 for i in range(10, 110)],
-        mode="text",
-        text=['|' for i in range(10, 110)],
-        textfont=dict(color="white",
-                      size=35,
-                      family="Courier New"),
-        showlegend=False,
-        name='hashmarks',
-        marker=dict(opacity=0),
-        # Making markers fully transparent
-    ))
 
     # Add colored endzones
     fig.add_shape(type="rect",
@@ -181,7 +164,18 @@ def animatePlay(games: pd.DataFrame, players: pd.DataFrame, plays: pd.DataFrame,
                   line=dict(color=nfl_teams_colors[visitingTeam][1]),
                   fillcolor=nfl_teams_colors[visitingTeam][0],
                   layer="below")
-    # Display the figure in Jupyter Notebook
+
+    # Add team names to endzones
+    fig.add_trace(go.Scatter(
+        x=[5, 115],
+        y=[27, 27],
+        mode="text",
+        text=[homeTeam, visitingTeam],
+        textfont=dict(color="white",
+                      size=30),
+        showlegend=False
+    ))
+
     fig.show()
 
 
